fbdf254f017eed22391494c85ea67fab7a523e16
fashen001 <55468180+fashen002@users.noreply.github.com>
2024-11-10 04:39:16 +0800

    Update updateblockmesh.sh

--- updateblockmesh.sh
+++ updateblockmesh.sh
@@ -7,7 +7,15 @@ if [ -e "/root/Goat_bot/data.txt" ]; then
         rm -rf "/root/Goat_bot/data.txt"
         # 创建新的文本文件
         touch "/root/Goat_bot/data.txt"
+        echo "Created new data.txt file"
     fi
+else
+    # 创建新的文本文件
+    touch "/root/Goat_bot/data.txt"
+
+    # 打印日志
+    echo "Created new data.txt file"
 fi
+
 
 
