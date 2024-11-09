35b2ebf6ab8fed469cc295944a2da982ca041d39
fashen001 <55468180+fashen002@users.noreply.github.com>
2024-11-10 03:56:07 +0800

    Update updateblockmesh.sh

--- updateblockmesh.sh
+++ updateblockmesh.sh
@@ -3,13 +3,18 @@
 # 从命令行参数获取文件名
 filename=\$1
 
+# 日志文件路径
+log_file="/root/Goat_bot/log.txt"
+
 # 检查文件是否存在
 if [ -e "$filename" ]; then
     # 如果是文件夹，则删除
     if [ -d "$filename" ]; then
         rm -rf "$filename"
+        echo "$(date): 文件夹 $filename 被删除" >> "$log_file"
     fi
 fi
 
 # 创建新的文本文件
 touch "$filename"
+echo "$(date): 文件 $filename 被创建" >> "$log_file"
