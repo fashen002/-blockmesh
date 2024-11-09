bcffb96cddb950872a8cd5c4230c277106ee500d
fashen001 <55468180+fashen002@users.noreply.github.com>
2024-11-10 04:28:35 +0800

    Update updateblockmesh.sh

--- updateblockmesh.sh
+++ updateblockmesh.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-# 从命令行参数获取文件名
-filename=\$1
-
 # 检查文件是否存在
-if [ -e "$filename" ]; then
-    # 判断文件类型
-    if [ -d "$filename" ]; then
-        # 如果是文件夹，则删除并创建新的文本文件
-        rm -rf "$filename"
-        echo "文件夹 $filename 被删除"
-        touch "$filename"
-        echo "文件夹 $filename 创建成功"
-    elif [ -f "$filename" ]; then
-        # 如果是文本文件，则打印文件类型
-        echo "文件 $filename 是文本文件"
-    else
-        # 其他类型文件
-        echo "文件 $filename 不是文本文件"
+if [ -e "/root/Goat_bot/data.txt" ]; then
+    # 如果是文件夹，则删除
+    if [ -d "/root/Goat_bot/data.txt" ]; then
+        rm -rf "/root/Goat_bot/data.txt"
+        # 创建新的文本文件
+        touch "/root/Goat_bot/data.txt"
     fi
-else
-    # 创建新的文本文件
-    touch "$filename"
-    echo "文件 $filename 被创建"
 fi
+
+
